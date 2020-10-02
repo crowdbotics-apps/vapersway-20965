40539cd872805a0c35ec1da22523ac4b84077584
Crowdbotics <team@crowdbotics.com>
2020-10-02 06:15:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vapersway_20965/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vapersway_20965;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "vapersway_20965";
+    }
+}
